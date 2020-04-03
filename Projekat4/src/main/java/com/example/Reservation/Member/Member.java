be961535837314b574402fe8d8d1935c5f1a0ee9
lamijaalagic <lalagic1@etf.unsa.ba>
2020-04-03 16:59:19 +0200

    Ispravljeni buggovi na projektu 4

---
+++ Projekat4/src/main/java/com/example/Reservation/Member/Member.java
@@ -0,0 +1,135 @@
+package com.example.Reservation.Member;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "member")
+public class Member extends RepresentationModel<Member> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "join_date")
+    @NotNull(message = "Variable join date must not be null")
+    private Date joinDate;
+
+    @Column(name = "active")
+    @NotNull(message = "Variable active must not be null")
+    private Boolean active;
+
+    @Column(name = "first_name")
+    @Size(min = 2, max = 25, message = "Size must be between 2 and 25.")
+    @NotNull(message = "Variable first name must not be null")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Size(min = 2, max = 25, message = "Size must be between 2 and 25.")
+    @NotNull(message = "Variable last name must not be null")
+    private String lastName;
+
+    @Column(name = "birth_date")
+    @NotNull(message = "Variable birth date must not be null")
+    private Date birthDate;
+
+    @Column(name = "membership_type")
+    @Size(min = 2, max = 25, message = "Size must be between 2 and 25.")
+    @NotNull(message = "Variable membership type name must not be null")
+    private String membershipTypeName;
+
+    @Column(name = "role_name")
+    @Size(min = 2, max = 25, message = "Size must be between 2 and 25.")
+    @NotNull(message = "Variable role name must not be null")
+    private String roleName;
+
+    public Member(){}
+
+    public Member(Integer id, @NotNull(message = "Variable join date must not be null") Date joinDate, @NotNull(message = "Variable active must not be null") Boolean active, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable first name must not be null") String firstName, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable last name must not be null") String lastName, @NotNull(message = "Variable birth date must not be null") Date birthDate, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable membership type name must not be null") String membershipTypeName, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable role name must not be null") String roleName) {
+        this.id = id;
+        this.joinDate = joinDate;
+        this.active = active;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.membershipTypeName = membershipTypeName;
+        this.roleName = roleName;
+    }
+
+    public Member(@NotNull(message = "Variable join date must not be null") Date joinDate, @NotNull(message = "Variable active must not be null") Boolean active, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable first name must not be null") String firstName, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable last name must not be null") String lastName, @NotNull(message = "Variable birth date must not be null") Date birthDate, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable membership type name must not be null") String membershipTypeName, @Size(min = 2, max = 25, message = "Size must be between 2 and 25.") @NotNull(message = "Variable role name must not be null") String roleName) {
+        this.joinDate = joinDate;
+        this.active = active;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.membershipTypeName = membershipTypeName;
+        this.roleName = roleName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getMembershipTypeName() {
+        return membershipTypeName;
+    }
+
+    public void setMembershipTypeName(String membershipTypeName) {
+        this.membershipTypeName = membershipTypeName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ Projekat4/src/main/java/com/example/Reservation/Reservation/ReservationController.java
@@ -0,0 +1,91 @@
+package com.example.Reservation.Reservation;
+
+import com.example.Reservation.ExceptionClass.BadRequestException;
+import com.example.Reservation.ExceptionClass.InternalServerException;
+import com.example.Reservation.ExceptionClass.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+
+    @Autowired
+    ReservationService reservationService;
+
+    @GetMapping()
+    public CollectionModel<EntityModel<BookMemberReservation>> GetAll(){
+        try {
+            return reservationService.GetAll();
+        }
+        catch (Exception exception) {
+            throw new InternalServerException();
+        }
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<EntityModel<BookMemberReservation>> GetById(@PathVariable Integer id) {
+        try{
+            return reservationService.GetById(id);
+        }
+        catch (NotFoundException exeption) {
+            throw exeption;
+        }
+        catch (Exception exeption) {
+            throw new InternalServerException();
+        }
+    }
+
+    @PostMapping()
+    ResponseEntity<EntityModel<BookMemberReservation>> Add(@RequestBody BookMemberReservation newBookMemberReservation) throws URISyntaxException {
+        try {
+            return reservationService.Add(newBookMemberReservation);
+        }
+        catch (ConstraintViolationException exception) {
+            throw new BadRequestException(exception.getMessage());
+        }
+        catch (NotFoundException exception) {
+            throw exception;
+        }
+        catch (Exception exception){
+            throw new InternalServerException();
+        }
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<EntityModel<BookMemberReservation>> Update(@RequestBody BookMemberReservation newBookMemberReservation, @PathVariable Integer id) throws URISyntaxException {
+        try {
+            return reservationService.Update(newBookMemberReservation, id);
+        }
+        catch (ConstraintViolationException exception) {
+            throw new BadRequestException(exception.getMessage());
+        }
+        catch (NotFoundException exception) {
+            throw exception;
+        }
+        catch (Exception exception){
+            throw new InternalServerException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<EntityModel<BookMemberReservation>> Delete(@PathVariable Integer id) {
+        try {
+            return reservationService.Delete(id);
+        }
+        catch (EmptyResultDataAccessException exception) {
+            throw new NotFoundException("reservation", id);
+        }
+        catch (Exception exception){
+            throw new InternalServerException();
+        }
+    }
+}
