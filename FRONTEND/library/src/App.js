b880280f60602e65418c4bd21828a594fe09c049
djureta1 <djureta1@etf.unsa.ba>
2020-05-27 21:43:27 +0200

    Role form

--- FRONTEND/library/src/App.js
+++ FRONTEND/library/src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Login from "./Login/Login"
 import Genre from "./Genre/Genre"
 import {Profile} from "./Profile/index"
+import {Role} from "./Role/index"
 
 
 function App() {
   return (
-    <Profile />
+    <Role />
   );
 }
 
